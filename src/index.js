41191a482537db05b56209bd1e175cb68c3c6868
Vulee-92 <52331023+Vulee-92@users.noreply.github.com>
2024-08-22 15:18:35 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 const express = require("express");
-const app = express();
-const http = require('http');
+const http = require("http");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
+const cors = require("cors");
 const routes = require("./routes");
-const bodyParser = require("body-parser");
-const OrderNotificationService = require('./services/OrderNotificationService');
-const helmet = require('helmet');
-const cors = require('cors'); // Thêm cors
+const OrderNotificationService = require("./services/OrderNotificationService");
+
 dotenv.config();
 
+const app = express();
 const port = process.env.PORT || 3001;
 
-// Sử dụng middleware body-parser để phân tích dữ liệu JSON
-app.use(bodyParser.json({ limit: "50mb" }));
+// CORS middleware
+app.use(
+  cors({
+    origin: ["https://www.hymnscenter.com", "https://hymnscenter.online"], // Thay bằng domain ứng dụng React của bạn
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true, // Cho phép gửi cookie và thông tin xác thực
+  })
+);
+app.options("*", cors()); // Kích hoạt preflight cho tất cả các route
 
-app.use(cors({
-  origin: ['https://your-react-app.com', 'https://hymnscenter.online'], // Thay bằng domain ứng dụng React của bạn
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true // Nếu bạn cần gửi cookie hoặc các thông tin xác thực
-}));
-app.options('*', cors()); // Kích hoạt preflight cho tất cả các route
+// Sử dụng Helmet để thêm các header bảo mật
+app.use(helmet());
+
+// Sử dụng middleware để phân tích dữ liệu JSON (thay thế body-parser)
+app.use(express.json({ limit: "50mb" }));
 
 // Thiết lập các route của ứng dụng
 routes(app);
@@ -29,9 +35,12 @@ routes(app);
 const server = http.createServer(app);
 OrderNotificationService.initSocket(server);
 
-// Thiết lập kết nối MongoDB
+// Kết nối MongoDB
 mongoose
-  .connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to MongoDB database");
   })
@@ -41,12 +50,12 @@ mongoose
 
 // Lắng nghe sự kiện thay đổi dữ liệu trong MongoDB và thông báo cho client qua Socket.IO
 const db = mongoose.connection;
-db.once('open', () => {
-  const collection = db.collection('comments');
+db.once("open", () => {
+  const collection = db.collection("comments");
   const changeStream = collection.watch();
-  changeStream.on('change', (change) => {
+  changeStream.on("change", (change) => {
     console.log(change);
-    server.io.emit('change', change);
+    server.io.emit("change", change);
   });
 });
 
@@ -54,6 +63,12 @@ db.once('open', () => {
 app.get("/", (req, res) => {
   res.setHeader("Cache-Control", "no-store"); // Không lưu trữ bản sao của trang web trong bộ nhớ cache
   res.send("Hello world!");
+});
+
+// Xử lý lỗi toàn cục
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Bắt đầu máy chủ và lắng nghe trên cổng đã chỉ định
