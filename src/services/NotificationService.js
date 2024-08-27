a73e18d9143c0f97b1404b861ab9885b18326252
Vulee-92 <52331023+Vulee-92@users.noreply.github.com>
2024-08-27 19:55:13 +0700

    update

--- src/services/NotificationService.js
+++ src/services/NotificationService.js
@@ -1,4 +1,4 @@
-const Order = require('../models/OrderModel'); // Thay thế với đường dẫn đúng đến model Order
+const Order = require('../models/OrderModel');
 let clients = [];
 let notificationQueue = [];
 let isSending = false;
@@ -8,18 +8,16 @@ const streamOrderNotifications = async (req, res) => {
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
 
-  // Thêm client vào danh sách để gửi dữ liệu
   clients.push(res);
 
-  // Khi client ngắt kết nối
   req.on('close', () => {
     clients = clients.filter(client => client !== res);
     if (clients.length === 0) {
-      clearNotificationQueue(); // Xóa hàng đợi khi không còn client nào kết nối
+      clearNotificationQueue();
     }
   });
 
-  // Thêm 7 giây delay trước khi gửi thông báo đầu tiên
+  // Thêm delay trước khi gửi thông báo đầu tiên
   setTimeout(() => {
     if (!isSending) {
       sendOrderNotifications();
@@ -31,10 +29,8 @@ const sendOrderNotifications = async () => {
   try {
     const orders = await Order.find().sort({ createdAt: -1, updatedAt: -1 });
 
-    // Nếu không có đơn hàng, không gửi thông báo
     if (!orders.length) return;
 
-    // Đưa tất cả các đơn hàng vào hàng đợi thông báo
     orders.forEach(order => {
       const notification = {
         fullName: order.shippingAddress.fullName,
@@ -42,14 +38,13 @@ const sendOrderNotifications = async () => {
         orderItems: order.orderItems.map(item => ({
           name: item.name,
           slug: item.slug,
-          image: item.image[0] // Lấy hình ảnh đầu tiên trong mảng
+          image: item.image[0]
         }))
       };
       notificationQueue.push(notification);
     });
 
-    // Bắt đầu gửi thông báo
-    if (!isSending) {
+    if (!isSending && notificationQueue.length > 0) {
       isSending = true;
       sendNotification();
     }
@@ -60,24 +55,22 @@ const sendOrderNotifications = async () => {
 
 const sendNotification = () => {
   if (!notificationQueue.length) {
-    isSending = false; // Dừng việc gửi nếu hàng đợi rỗng
+    isSending = false;
     return;
   }
 
-  const notification = notificationQueue.shift(); // Lấy thông báo đầu tiên trong hàng đợi
+  const notification = notificationQueue.shift();
 
-  // Gửi thông báo cho tất cả các client
   clients.forEach(client => {
     client.write(`data: ${JSON.stringify(notification)}\n\n`);
   });
 
-  // Tạo thời gian ngẫu nhiên cho lần gửi thông báo tiếp theo
-  const minDelay = 5000; // 7 seconds
-  const maxDelay = 20000; // 15 seconds
+  const minDelay = 7000; // 7 seconds
+  const maxDelay = 15000; // 15 seconds
 
   const randomTime = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
 
-  setTimeout(sendNotification, randomTime); // Tiếp tục gửi thông báo sau thời gian delay ngẫu nhiên
+  setTimeout(sendNotification, randomTime);
 };
 
 const clearNotificationQueue = () => {
